5bb37f19d638b5291560a23e44aa47856c232d28
Marla <80172917+edistomars@users.noreply.github.com>
2022-02-14 16:41:50 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./dictionary.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid rounded-3" alt="logo" />
+        <h1 className="text-center">Dictionary</h1>
+      </header>
+    </div>
+  );
+}
