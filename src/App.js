933a1d75f71353a6473bd146d86e95f71f377f4f
Marla <80172917+edistomars@users.noreply.github.com>
2022-02-15 15:36:33 -0500

    created a new component of which has a state, component returns a form that when submitted, searches and pop ups an alert

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from "./dictionary.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid rounded-3" alt="logo" />
-        <h1 className="text-center">Dictionary</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid rounded-3" alt="logo" />
+          <h1 className="text-center">Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Marla Laystrom</small>
+        </footer>
+      </div>
     </div>
   );
 }
